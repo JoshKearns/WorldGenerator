fc93fdf933c92853b72503823079140497032288
JoshKearns <61230934+JoshKearns@users.noreply.github.com>
2020-12-23 17:17:14 +0000

    Player Controller

---
+++ WorldGenerator/Assets/MouseLook.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    // Look speed
+    public float mouseSensitivity = 100f;
+    private float xRotation = 0f;
+    
+    // Player body reference
+    public Transform player;
+    
+    // Is player holding item
+    public bool holding;
+    
+    void Start()
+    {
+        holding = false;
+        // Hide cursor
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        // Get controller input
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Lock vertical look to 180 Degrees
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        // Look up & down
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        // Look left & right
+        player.Rotate(Vector3.up * mouseX);
+        
+    }
+}
---
+++ WorldGenerator/Assets/PlayerMovement.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Movement
+    public CharacterController controller;
+    public float speed = 12;
+    public float jumpHeight = 3f;
+
+    // Ground check
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    
+    // Gravity
+    public float gravity = -9.81f;
+    private Vector3 velocity;
+    
+    //Under Water
+    public bool underWater = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get controller input
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            
+            // Calculate move direction
+            Vector3 move = transform.right * x + transform.forward * z;
+            
+            if (!underWater)
+            {
+                // Ground check
+                isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+                // Reset Velocity if grounded
+                if (isGrounded && velocity.y < 0)
+                {
+                    velocity.y = -2f;
+                }
+                
+                // Gravity
+                velocity.y += gravity * Time.deltaTime;
+                controller.Move(velocity* Time.deltaTime);
+        
+                // Jump
+                if (Input.GetButtonDown("Jump") && isGrounded)
+                {
+                    velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                }
+                
+                // Movement
+                controller.Move(move * speed * Time.deltaTime);
+            }
+            else
+            {
+                // Movement
+                controller.Move((move *.75f) * speed * Time.deltaTime);
+
+                // Gravity
+                velocity.y += gravity * Time.deltaTime;
+                controller.Move(velocity /2 * Time.deltaTime);
+        
+                // Jump
+                if (Input.GetButton("Jump"))
+                {
+                    velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                }
+            }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Water"))
+        {
+            underWater = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Water"))
+        {
+            underWater = false;
+        }
+    }
+}
