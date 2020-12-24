543cd9986ee0e8e9879038c66de396cbb32dfbce
JoshKearns <61230934+JoshKearns@users.noreply.github.com>
2020-12-24 17:03:34 +0000

    0

--- WorldGenerator/Assets/Scripts/MouseLook.cs
+++ WorldGenerator/Assets/Scripts/MouseLook.cs
@@ -10,32 +10,36 @@ public class MouseLook : MonoBehaviour
     
     // Player body reference
     public Transform player;
-    
-    // Is player holding item
-    public bool holding;
+
+    private bool _paused;
+    private PlayerMovement _playerMovement;
     
     void Start()
     {
-        holding = false;
+        _paused = false;
+        _playerMovement = gameObject.transform.parent.GetComponent<PlayerMovement>();
         // Hide cursor
         //Cursor.lockState = CursorLockMode.Locked;
     }
     
     void Update()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        // Get controller input
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * MenuScript.globalMouse * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * MenuScript.globalMouse * Time.deltaTime;
+        _paused = _playerMovement.paused;
+        if (!_paused)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            // Get controller input
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * MenuScript.globalMouse * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * MenuScript.globalMouse * Time.deltaTime;
 
-        // Lock vertical look to 180 Degrees
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            // Lock vertical look to 180 Degrees
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         
-        // Look up & down
-        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-        // Look left & right
-        player.Rotate(Vector3.up * mouseX);
-        
+            // Look up & down
+            transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+            // Look left & right
+            player.Rotate(Vector3.up * mouseX);
+        }
     }
 }
