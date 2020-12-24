7b7551219be779eb786181b31d7efc44ac4fb3d1
JoshKearns <61230934+JoshKearns@users.noreply.github.com>
2020-12-24 14:36:07 +0000

    Coin

---
+++ WorldGenerator/Assets/BillboardScript.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardScript : MonoBehaviour
+{
+    private GameObject _player;
+
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        transform.LookAt(transform.position + _player.transform.rotation * Vector3.forward, _player.transform.rotation * Vector3.up);
+        var eulerAngles = transform.eulerAngles;
+        eulerAngles.z = 0;
+        transform.eulerAngles = eulerAngles;
+    }
+}
---
+++ WorldGenerator/Assets/CoinAnimate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinAnimate : MonoBehaviour
+{
+    public Sprite[] coinSprites;
+    private SpriteRenderer _rend;
+    private int _counter = 0;
+
+    private void Start()
+    {
+        _rend = GetComponentInChildren<SpriteRenderer>();
+        StartCoroutine(CoinAnimateFunction());
+    }
+
+    IEnumerator CoinAnimateFunction()
+    {
+        if (_counter < coinSprites.Length - 1)
+            _counter++;
+        else
+            _counter = 0;
+        yield return new WaitForSeconds(.1f);
+        _rend.sprite = coinSprites[_counter];
+        StartCoroutine(CoinAnimateFunction());
+    }
+}
